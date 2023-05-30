6d79f134dde7f1292886a67cd7bad57984f8b69a
JavohirJalilov <javohirjaliloff@gmail.com>
2023-05-30 06:43:31 +0500

    package

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
    name='basic_function',
    version='0.1.0',
-   packages=['basicFunction'],
+   packages=['basic_function'],
    description='This is Basic Function Test',
    install_requires=[
        "requests",
